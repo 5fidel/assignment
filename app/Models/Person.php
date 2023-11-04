1dd564ec560e9546b755830f9070379fc083d177
5fidel <flip5field@gmail.com>
2023-11-04 17:42:44 +0000

    Minor Typo in Person.php

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -11,7 +11,7 @@ class Person extends Model
 
     protected $fillable = ['name'];
 
-    public function building()
+    public function buildings()
     {
         return $this->belongsTo(Building::class);
     }
